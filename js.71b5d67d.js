be6b39a982366fdfe802d6e526d14abdfb45e4d2
Vladimir Rodkin <mail@vovanr.com>
2019-04-22 17:40:06 +0300

    Updates

--- js.71b5d67d.js
+++ js.71b5d67d.js
@@ -1691,7 +1691,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52414" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51570" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
